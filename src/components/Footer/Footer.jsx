359781c672e8574bffdfd58faace4a47de5f1fd7
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-06-21 18:50:28 +0530

    created footer for mobile screen

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import image from "../../Assets/image.jpg";
+import { FiSend } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <section className="footer">
+      <div className="videoDiv">
+        {/* replace below image with video in future */}
+        <img src={image} alt="" />
+      </div>
 
-export default Footer
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter email address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
