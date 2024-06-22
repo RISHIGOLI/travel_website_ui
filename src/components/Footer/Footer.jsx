f24fc033de7daa1507da752039223d605641f232
RISHI GOLI <rg0001.ayconnects@gmail.com>
2024-06-22 13:32:53 +0530

    added aos animations to all sections to feel better UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./footer.css";
 import image from "../../Assets/image.jpg";
-import { FiSend } from "react-icons/fi";
+import { FiChevronRight, FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import {
+  AiFillInstagram,
+  AiFillYoutube,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Footer = () => {
+  useEffect(()=>{
+    Aos.init({duration:2000})
+  },[])
   return (
     <section className="footer">
       <div className="videoDiv">
@@ -13,17 +25,140 @@ const Footer = () => {
 
       <div className="secContent container">
         <div className="contactDiv flex">
-          <div className="text">
+          <div data-aos="fade-up" className="text">
             <small>KEEP IN TOUCH</small>
             <h2>Travel with us</h2>
           </div>
 
           <div className="inputDiv flex">
-            <input type="text" placeholder="Enter email address" />
-            <button className="btn flex" type="submit">
+            <input data-aos="fade-up" type="text" placeholder="Enter email address" />
+            <button data-aos="fade-up" className="btn flex" type="submit">
               SEND <FiSend className="icon" />
             </button>
           </div>
+        </div>
+
+        <div className="footerCard flex">
+
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdOutlineTravelExplore className="icon" /> Travel.
+              </a>
+            </div>
+
+            <div data-aos="fade-up" className="footerParagraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Voluptatum cumque corrupti necessitatibus illum pariatur aut
+              totam, quia deserunt quam fuga quos atque tempore accusamus et?
+              Cupiditate iusto fugit optio sit.
+            </div>
+
+            <div data-aos="fade-up" className="footerSocials flex">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+
+          <div data-aos="fade-up" className="footerLinks grid">
+            {/* group one */}
+            <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Service
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+
+            {/* group two */}
+            <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Hostelworld
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+
+            {/* group three */}
+            <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Londan
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+
+          <div className="footerDiv flex">
+            <small>BEST TRAVEL WEBSITE THEME</small>
+            <small>COPYRIGHTS RESERVED - ISRATECH 2022</small>
+          </div>
+
         </div>
       </div>
     </section>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./main.css";
 import image from "../../Assets/image.jpg";
 import { HiOutlineClipboardCheck, HiOutlineLocationMarker } from "react-icons/hi";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const data = [
   {
@@ -96,10 +98,13 @@ const data = [
   },
 ];
 const Main = () => {
+  useEffect(()=>{
+    Aos.init({duration:2000})
+  },[])
   return (
     <section className="main container section">
       <div className="secTitle">
-        <h3 className="title">Most visited destinations</h3>
+        <h3 data-aos="fade-right" className="title">Most visited destinations</h3>
       </div>
 
       <div className="secContent grid">
@@ -107,7 +112,7 @@ const Main = () => {
         {data.map(
           ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
             return (
-              <div className="singleDestination" key={id}>
+              <div data-aos="fade-up" className="singleDestination" key={id}>
                 {/* here it will return single id from the map array */}
                 <div className="imageDiv">
                   <img src={imgSrc} alt={destTitle} />
